d0ae193059bb70e9759f6a0d9d42fea4874ca98f
Jordan Marques <jordanmarques1994@gmail.com>
2017-12-14 15:15:01 +0100

    add datas

---
+++ src/assets/data/hiraganas.js
@@ -0,0 +1,437 @@
+[
+  {
+    "kana": "あ",
+    "alphabet": "a",
+    "base": [
+      {
+        "kana": "か",
+        "alphabet": "ka"
+      },
+      {
+        "kana": "さ",
+        "alphabet": "sa"
+      },
+      {
+        "kana": "た",
+        "alphabet": "ta"
+      },
+      {
+        "kana": "な",
+        "alphabet": "na"
+      },
+      {
+        "kana": "は",
+        "alphabet": "ha"
+      },
+      {
+        "kana": "ま",
+        "alphabet": "ma"
+      },
+      {
+        "kana": "や",
+        "alphabet": "ya"
+      },
+      {
+        "kana": "ら",
+        "alphabet": "ra"
+      },
+      {
+        "kana": "わ",
+        "alphabet": "wa"
+      },
+      {
+        "kana": "が",
+        "alphabet": "ga"
+      },
+      {
+        "kana": "ざ",
+        "alphabet": "za"
+      },
+      {
+        "kana": "だ",
+        "alphabet": "da"
+      },
+      {
+        "kana": "ば",
+        "alphabet": "ba"
+      },
+      {
+        "kana": "ぱ",
+        "alphabet": "pa"
+      },
+      {
+        "kana": "きゃ",
+        "alphabet": "kya"
+      },
+      {
+        "kana": "ぎゃ",
+        "alphabet": "gya"
+      },
+      {
+        "kana": "しゃ",
+        "alphabet": "sha"
+      },
+      {
+        "kana": "じゃ",
+        "alphabet": "ja"
+      },
+      {
+        "kana": "ちゃ",
+        "alphabet": "cha"
+      },
+      {
+        "kana": "にゃ",
+        "alphabet": "nya"
+      },
+      {
+        "kana": "ひゃ",
+        "alphabet": "hya"
+      },
+      {
+        "kana": "びゃ",
+        "alphabet": "bya"
+      },
+      {
+        "kana": "ぴゃ",
+        "alphabet": "pya"
+      },
+      {
+        "kana": "みゃ",
+        "alphabet": "mya"
+      },
+      {
+        "kana": "りゃ",
+        "alphabet": "rya"
+      }
+    ]
+  },
+  {
+    "kana": "い",
+    "alphabet": "i",
+    "base": [
+      {
+        "kana": "き",
+        "alphabet": "ki"
+      },
+      {
+        "kana": "し",
+        "alphabet": "shi"
+      },
+      {
+        "kana": "ち",
+        "alphabet": "chi"
+      },
+      {
+        "kana": "に",
+        "alphabet": "ni"
+      },
+      {
+        "kana": "ひ",
+        "alphabet": "hi"
+      },
+      {
+        "kana": "み",
+        "alphabet": "mi"
+      },
+      {
+        "kana": "り",
+        "alphabet": "ri"
+      },
+      {
+        "kana": "ゐ",
+        "alphabet": "wi"
+      },
+      {
+        "kana": "ぎ",
+        "alphabet": "gi"
+      },
+      {
+        "kana": "じ",
+        "alphabet": "ji"
+      },
+      {
+        "kana": "ぢ",
+        "alphabet": "ji(di)"
+      },
+      {
+        "kana": "び",
+        "alphabet": "bi"
+      },
+      {
+        "kana": "ぴ",
+        "alphabet": "pi"
+      }
+    ]
+  },
+  {
+    "kana": "う",
+    "alphabet": "u",
+    "base": [
+      {
+        "kana": "く",
+        "alphabet": "ku"
+      },
+      {
+        "kana": "す",
+        "alphabet": "su"
+      },
+      {
+        "kana": "つ",
+        "alphabet": "tsu"
+      },
+      {
+        "kana": "ぬ",
+        "alphabet": "nu"
+      },
+      {
+        "kana": "ふ",
+        "alphabet": "fu"
+      },
+      {
+        "kana": "む",
+        "alphabet": "mu"
+      },
+      {
+        "kana": "ゆ",
+        "alphabet": "yu"
+      },
+      {
+        "kana": "る",
+        "alphabet": "ru"
+      },
+      {
+        "kana": "ぐ",
+        "alphabet": "gu"
+      },
+      {
+        "kana": "ず",
+        "alphabet": "zu"
+      },
+      {
+        "kana": "づ",
+        "alphabet": "zu(du)"
+      },
+      {
+        "kana": "ぶ",
+        "alphabet": "bu"
+      },
+      {
+        "kana": "ぷ",
+        "alphabet": "pu"
+      },
+      {
+        "kana": "きゅ",
+        "alphabet": "kyu"
+      },
+      {
+        "kana": "ぎゅ",
+        "alphabet": "gyu"
+      },
+      {
+        "kana": "しゅ",
+        "alphabet": "shu"
+      },
+      {
+        "kana": "じゅ",
+        "alphabet": "ju"
+      },
+      {
+        "kana": "ちゅ",
+        "alphabet": "chu"
+      },
+      {
+        "kana": "にゅ",
+        "alphabet": "nyu"
+      },
+      {
+        "kana": "ひゅ",
+        "alphabet": "hyu"
+      },
+      {
+        "kana": "びゅ",
+        "alphabet": "byu"
+      },
+      {
+        "kana": "ぴゅ",
+        "alphabet": "pyu"
+      },
+      {
+        "kana": "みゅ",
+        "alphabet": "myu"
+      },
+      {
+        "kana": "りゅ",
+        "alphabet": "ryu"
+      }
+    ]
+  },
+  {
+    "kana": "え",
+    "alphabet": "e",
+    "base": [
+      {
+        "kana": "け",
+        "alphabet": "ke"
+      },
+      {
+        "kana": "せ",
+        "alphabet": "se"
+      },
+      {
+        "kana": "て",
+        "alphabet": "te"
+      },
+      {
+        "kana": "ね",
+        "alphabet": "ne"
+      },
+      {
+        "kana": "へ",
+        "alphabet": "he"
+      },
+      {
+        "kana": "め",
+        "alphabet": "me"
+      },
+      {
+        "kana": "れ",
+        "alphabet": "re"
+      },
+      {
+        "kana": "ゑ",
+        "alphabet": "we"
+      },
+      {
+        "kana": "げ",
+        "alphabet": "ge"
+      },
+      {
+        "kana": "ぜ",
+        "alphabet": "ze"
+      },
+      {
+        "kana": "で",
+        "alphabet": "de"
+      },
+      {
+        "kana": "べ",
+        "alphabet": "be"
+      },
+      {
+        "kana": "ぺ",
+        "alphabet": "pe"
+      }
+    ]
+  },
+  {
+    "kana": "お",
+    "alphabet": "o",
+    "base": [
+      {
+        "kana": "こ",
+        "alphabet": "ko"
+      },
+      {
+        "kana": "そ",
+        "alphabet": "so"
+      },
+      {
+        "kana": "と",
+        "alphabet": "to"
+      },
+      {
+        "kana": "の",
+        "alphabet": "no"
+      },
+      {
+        "kana": "ほ",
+        "alphabet": "ho"
+      },
+      {
+        "kana": "も",
+        "alphabet": "mo"
+      },
+      {
+        "kana": "よ",
+        "alphabet": "yo"
+      },
+      {
+        "kana": "ろ",
+        "alphabet": "ro"
+      },
+      {
+        "kana": "を",
+        "alphabet": "wo"
+      },
+      {
+        "kana": "ん",
+        "alphabet": "n"
+      },
+      {
+        "kana": "ご",
+        "alphabet": "go"
+      },
+      {
+        "kana": "ぞ",
+        "alphabet": "zo"
+      },
+      {
+        "kana": "ど",
+        "alphabet": "do"
+      },
+      {
+        "kana": "ぼ",
+        "alphabet": "bo"
+      },
+      {
+        "kana": "ぽ",
+        "alphabet": "po"
+      },
+      {
+        "kana": "きょ",
+        "alphabet": "kyo"
+      },
+      {
+        "kana": "ぎょ",
+        "alphabet": "gyo"
+      },
+      {
+        "kana": "しょ",
+        "alphabet": "sho"
+      },
+      {
+        "kana": "じょ",
+        "alphabet": "jo"
+      },
+      {
+        "kana": "ちょ",
+        "alphabet": "cho"
+      },
+      {
+        "kana": "にょ",
+        "alphabet": "nyo"
+      },
+      {
+        "kana": "ひょ",
+        "alphabet": "hyo"
+      },
+      {
+        "kana": "びょ",
+        "alphabet": "byo"
+      },
+      {
+        "kana": "ぴょ",
+        "alphabet": "pyo"
+      },
+      {
+        "kana": "みょ",
+        "alphabet": "myo"
+      },
+      {
+        "kana": "く",
+        "alphabet": "ku"
+      }
+
+    ]
+  }
+]
